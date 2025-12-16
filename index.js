c13d32f5306ea609ba647b141dc7e8a1aca1af5d
Cyber_k.ing <163102570+CYBERKING17@users.noreply.github.com>
2025-12-16 23:14:59 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-var typed= new Typed(".text", {
-    strings: ["Web Developer" , "Full Stack Developer" , "Java developer"],
-    typespeed: 100,
-    backspeed: 100,
-    backDelay: 1000,
-    loop: true
-})
